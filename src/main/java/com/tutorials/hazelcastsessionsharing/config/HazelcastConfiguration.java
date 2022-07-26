8eed0f8c6611835d6d6402e711759da4b953be3d
LAGHRISSI Imad <laghrissi.imad@gmail.com>
2022-07-26 07:59:12 +0200

    session sharing works for simple sign in of two instances

--- src/main/java/com/tutorials/hazelcastsessionsharing/config/HazelcastConfiguration.java
+++ src/main/java/com/tutorials/hazelcastsessionsharing/config/HazelcastConfiguration.java
@@ -1,37 +1,24 @@
 package com.tutorials.hazelcastsessionsharing.config;
 
-import com.hazelcast.client.HazelcastClient;
-import com.hazelcast.client.config.ClientConfig;
-import com.hazelcast.client.config.ClientUserCodeDeploymentConfig;
 import com.hazelcast.config.AttributeConfig;
 import com.hazelcast.config.Config;
 import com.hazelcast.config.IndexConfig;
 import com.hazelcast.config.IndexType;
-import com.hazelcast.config.IntegrityCheckerConfig;
-import com.hazelcast.config.UserCodeDeploymentConfig;
 import com.hazelcast.core.Hazelcast;
 import com.hazelcast.core.HazelcastInstance;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.web.savedrequest.DefaultSavedRequest;
-//import org.springframework.security.web.savedrequest.SavedRequest;
-import org.springframework.security.web.savedrequest.DefaultSavedRequest;
 import org.springframework.session.FlushMode;
-import org.springframework.session.MapSession;
 import org.springframework.session.SaveMode;
-import org.springframework.session.Session;
 import org.springframework.session.config.SessionRepositoryCustomizer;
 import org.springframework.session.hazelcast.Hazelcast4IndexedSessionRepository;
 import org.springframework.session.hazelcast.Hazelcast4PrincipalNameExtractor;
-import org.springframework.session.hazelcast.Hazelcast4SessionUpdateEntryProcessor;
 import org.springframework.session.hazelcast.config.annotation.SpringSessionHazelcastInstance;
 import org.springframework.session.hazelcast.config.annotation.web.http.EnableHazelcastHttpSession;
 
 @Configuration
 @EnableHazelcastHttpSession
-@Slf4j
-public class SessionConfiguration {
+public class HazelcastConfiguration {
 
   private final String SESSIONS_MAP_NAME = "crp-session-map";
 
@@ -39,25 +26,23 @@ public class SessionConfiguration {
   @SpringSessionHazelcastInstance
   public HazelcastInstance hazelcastInstance() {
     Config config = new Config();
-    config.setClusterName("hello-world");
+    config.setClusterName("dev");
 
-    // Add this attribute to be able to query sessions by their PRINCIPAL_NAME_ATTRIBUTE's
     AttributeConfig attributeConfig = new AttributeConfig()
         .setName(Hazelcast4IndexedSessionRepository.PRINCIPAL_NAME_ATTRIBUTE)
         .setExtractorClassName(Hazelcast4PrincipalNameExtractor.class.getName());
-    config.setIntegrityCheckerConfig(new IntegrityCheckerConfig().setEnabled(true));
-    // Configure the sessions map
+
     config.getMapConfig(SESSIONS_MAP_NAME)
         .addAttributeConfig(attributeConfig).addIndexConfig(
             new IndexConfig(IndexType.HASH, Hazelcast4IndexedSessionRepository.PRINCIPAL_NAME_ATTRIBUTE));
 
-    /*config.getUserCodeDeploymentConfig()
-        .setEnabled(true)
-        .setClassCacheMode(UserCodeDeploymentConfig.ClassCacheMode.ETERNAL)
-        .setProviderMode(UserCodeDeploymentConfig.ProviderMode.LOCAL_AND_CACHED_CLASSES)
-        .setBlacklistedPrefixes("com.arval.crp")
-        //.setWhitelistedPrefixes("com.bar.MyClass")
-        .setProviderFilter("HAS_ATTRIBUTE:lite");*/
+    config.getNetworkConfig().getJoin().getTcpIpConfig().setEnabled(true);
+    config.getNetworkConfig().getJoin().getTcpIpConfig().addMember("192.168.1.78");
+
+  /*  config.getNetworkConfig().getJoin().getMulticastConfig().setEnabled(false);
+    config.getNetworkConfig().getJoin().getKubernetesConfig().setEnabled(true)
+        .setProperty("namespace", "development")
+        .setProperty("service-name", "hz-service");*/
 
     return Hazelcast.newHazelcastInstance(config);
   }
